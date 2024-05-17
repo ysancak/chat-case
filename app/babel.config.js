66ab5a713d2c451d3b4cc87c41631c04d7772681
Yusuf Sancak <yusuf@4lab>
2024-05-17 23:53:51 +0300

    adding module resolver

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+    [
+      "@babel/plugin-transform-private-methods", 
+      { "loose": true }
+    ]
+  ],
 };
