2968f85690002e07fd9f81cf3a99f1336029c49d
Yusuf Sancak <yusuf@4lab>
2024-05-18 19:00:58 +0300

    updating backend route

--- backend/routes/chat.ts
+++ backend/routes/chat.ts
@@ -4,26 +4,44 @@ import OpenAI from 'openai';
 
 dotenv.config();
 
-const router = express.Router()
+const router = express.Router();
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
 router.post('/', async (req: Request, res: Response) => {
-  const {messages} = req.body
+  const headers = {
+    'Content-Type': 'text/event-stream',
+    'Access-Control-Allow-Origin': '*',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'no-cache'
+  };
+  res.writeHead(200, headers);
+
+  const { messages } = req.body;
   const lastMessages = messages.slice(-10);
 
-  const stream = await openai.chat.completions.create({
-    model: 'gpt-4',
-    messages: lastMessages,
-    stream: true,
-  });
+  try {
+    const stream = await openai.chat.completions.create({
+      model: 'gpt-4',
+      messages: lastMessages,
+      stream: true,
+    });
 
-  for await (const chunk of stream) {
-    res.write(chunk.choices[0]?.delta?.content || '');
+    for await (const chunk of stream) {
+      let content = chunk.choices[0]?.delta?.content || ' ';
+      content = content.replace(/ /g, '\\s');
+      res.write(`data: ${content}\n\n`);
+      res.flush();
+    }
+
+    res.write(`data: [END]\n\n`);
+
+    res.end();
+  } catch (error) {
+    res.status(500).send('Internal Server Error');
   }
+});
 
-  res.end();
-})
-
-export default router
+export default router;
--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+import cors from 'cors';
+import compression from 'compression';
+
 import chatRoutes from './routes/chat';
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(cors());
+app.use(compression());
+
 app.use("/chat", chatRoutes)
 
 app.listen(port, () => {
